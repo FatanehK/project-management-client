66e33f8c1fefa6948cd700015ebaf5ded5daae4a
fataneh <fkarandish1@gmail.com>
2023-01-30 21:19:06 -0800

    implementing project detail page

--- src/Pages/ProjectDetail.tsx
+++ src/Pages/ProjectDetail.tsx
@@ -1,11 +1,115 @@
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { Alert, Box, Button, Snackbar, Stack, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { GetProject, SaveProject } from "../services/requestHandlers";
+import { IProject } from "../types";
 
 export const ProjectDetail: React.FC = () => {
-  const {projectId} = useParams();
+  const { projectId } = useParams();
+  const [project, setProject] = useState<Partial<IProject>>({
+    title: "",
+    description: "",
+  });
+  const [alert, setAlert] = useState<{
+    open: boolean;
+    severity: "success" | "error";
+  }>({
+    open: false,
+    severity: "error",
+  });
+
+  useEffect(() => {
+    const loadProject = async () => {
+      if (projectId) {
+        const prj = await GetProject(projectId);
+        if (prj) {
+          setProject(prj);
+        }
+      }
+    };
+    loadProject();
+  }, [projectId]);
+
+  const handleOnTitleChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const title = event.currentTarget.value;
+    setProject({ ...project, title });
+  };
+
+  const handleOnDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const description = event.currentTarget.value;
+    setProject({ ...project, description });
+  };
+
+  const onSave = async () => {
+    try {
+      await SaveProject(project);
+      setAlert({
+        open: true,
+        severity: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        severity: "error",
+      });
+    }
+  };
+
+  const handleCloseAlert = () => {
+    setAlert({ open: false, severity: alert.severity });
+  };
+
   return (
     <Box>
-      <div>{`ProjectId: ${projectId}`}</div>
+      <Snackbar
+        open={alert.open}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+      >
+        <Alert
+          onClose={handleCloseAlert}
+          severity={alert.severity}
+          sx={{ width: "100%" }}
+        >
+          {alert.severity === "success"
+            ? "Project information saved successfully!"
+            : "There was an error in saving the project information."}
+        </Alert>
+      </Snackbar>
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          p: 5,
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+      >
+        <TextField
+          label="Title"
+          variant="outlined"
+          value={project.title}
+          onChange={handleOnTitleChange}
+        />
+        <TextField
+          label="Desciption"
+          variant="outlined"
+          multiline
+          rows={5}
+          value={project.description}
+          onChange={handleOnDescriptionChange}
+        />
+        <Button variant="contained" sx={{ width: 100 }} onClick={onSave}>
+          Save
+        </Button>
+      </Box>
     </Box>
   );
 };
--- src/services/requestHandlers.ts
+++ src/services/requestHandlers.ts
@@ -5,19 +5,38 @@ const URL = "http://127.0.0.1:5000";
 
 export const GetProjects = async (adminId: number) => {
     let response: Array<IProject> = []
-    try {
-        const request = await axios({
-            method: 'get',
-            url: `${URL}/projects`,
-            responseType: 'json',
-            params: {
-                adminId
-            }
-        })
-        response = request.data
-    } catch (error) {
-        console.log(error);
-    }
+    const request = await axios({
+        method: 'get',
+        url: `${URL}/projects`,
+        responseType: 'json',
+        params: {
+            adminId
+        }
+    })
+    response = request.data
     return response
 };
+
+export const GetProject = async (projectId: string) => {
+    let response: IProject | null = null
+    const request = await axios({
+        method: 'get',
+        url: `${URL}/projects/${projectId}`,
+        responseType: 'json',
+    })
+    response = request.data
+    return response
+}
+
+export const SaveProject = async (project: Partial<IProject>) => {
+    const request = await axios({
+        method: 'put',
+        url: `${URL}/projects/${project.id}`,
+        responseType: 'json',
+        data: project
+    })
+    if (request.status !== 200) {
+        throw Error("Error in saving project")
+    }
+}
 
